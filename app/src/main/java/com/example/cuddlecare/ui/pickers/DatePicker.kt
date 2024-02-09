25fe4c419a90ad578cd6f02713f17d3c22350208
Adanwa Nwobodo <adanwanwobodo@gmail.com>
2024-02-08 19:28:52 -0800

    Implemented custom time picker and time picker dialog

---
+++ app/src/main/java/com/example/cuddlecare/ui/pickers/DatePicker.kt
@@ -0,0 +1,87 @@
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CalendarMonth
+import androidx.compose.material3.Button
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDatePickerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import java.time.Instant
+import java.time.LocalDate
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun CustomDatePicker() {
+    val date = remember { mutableStateOf(LocalDate.now()) }
+    val isOpen = remember { mutableStateOf(false) }
+
+    Row(verticalAlignment = Alignment.CenterVertically) {
+
+        OutlinedTextField(
+            readOnly = true,
+            value = date.value.format(DateTimeFormatter.ISO_DATE),
+            label = { Text("Date") },
+            onValueChange = {})
+
+        IconButton(
+            onClick = { isOpen.value = true } // show de dialog
+        ) {
+            Icon(imageVector = Icons.Default.CalendarMonth, contentDescription = "Calendar")
+        }
+    }
+
+    if (isOpen.value) {
+        CustomDatePickerDialog(
+            onAccept = {
+                isOpen.value = false // close dialog
+
+                if (it != null) { // Set the date
+                    date.value = Instant
+                        .ofEpochMilli(it)
+                        .atZone(ZoneId.of("UTC"))
+                        .toLocalDate()
+                }
+            },
+            onCancel = {
+                isOpen.value = false //close dialog
+            }
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CustomDatePickerDialog(
+    onAccept: (Long?) -> Unit,
+    onCancel: () -> Unit
+) {
+    val state = rememberDatePickerState()
+
+    DatePickerDialog(
+        onDismissRequest = { },
+        confirmButton = {
+            Button(onClick = { onAccept(state.selectedDateMillis) }) {
+                Text("Accept")
+            }
+        },
+        dismissButton = {
+            Button(onClick = onCancel) {
+                Text("Cancel")
+            }
+        }
+    ) {
+        DatePicker(state = state)
+    }
+}
