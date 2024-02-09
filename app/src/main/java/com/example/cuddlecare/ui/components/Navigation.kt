1f8c990c46b3a4f6dd4c9a754bf9b1da3d56b1c4
Adanwa Nwobodo <adanwanwobodo@gmail.com>
2024-02-08 22:52:37 -0800

    change the arrow icon to the recomended one

--- app/src/main/java/com/example/cuddlecare/ui/components/Navigation.kt
+++ app/src/main/java/com/example/cuddlecare/ui/components/Navigation.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
