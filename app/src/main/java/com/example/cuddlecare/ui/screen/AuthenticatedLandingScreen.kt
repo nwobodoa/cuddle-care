bb6b99daae877a237a92edfbbdea5e2798f786a6
Adanwa Nwobodo <adanwanwobodo@gmail.com>
2024-02-08 22:48:27 -0800

    created the authentication page but had to comment it out

--- app/src/main/java/com/example/cuddlecare/ui/screen/AuthenticatedLandingScreen.kt
+++ app/src/main/java/com/example/cuddlecare/ui/screen/AuthenticatedLandingScreen.kt
@@ -1,36 +1,37 @@
-package com.example.cuddlecare.ui.screen
-
-import android.app.Application
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import com.example.cuddlecare.ui.auth.FirebaseAuthViewModel
-
-enum class AuthenticatedLandingScreen {
-    setup,
-    Application
-}
-@Composable
-fun AuthenticatedLandingScreen(
-    firebaseAuthViewModel: FirebaseAuthViewModel = viewModel(),
-){
-//    val profileUiState by profileViewmodel.profileUIState.collectAsState()
+//package com.example.cuddlecare.ui.screen
+//TODO finish of the implemnetation
+//import android.app.Application
+//import androidx.compose.foundation.layout.Arrangement
+//import androidx.compose.foundation.layout.Column
+//import androidx.compose.foundation.layout.fillMaxSize
+//import androidx.compose.foundation.layout.padding
+//import androidx.compose.material3.CircularProgressIndicator
+//import androidx.compose.runtime.Composable
+//import androidx.compose.runtime.LaunchedEffect
+//import androidx.compose.ui.Alignment
+//import androidx.compose.ui.Modifier
+//import androidx.compose.ui.unit.dp
+//import androidx.lifecycle.viewmodel.compose.viewModel
+//import androidx.navigation.NavHostController
+//import androidx.navigation.compose.NavHost
+//import androidx.navigation.compose.composable
+//import androidx.navigation.compose.rememberNavController
+//import com.example.cuddlecare.ui.auth.FirebaseAuthViewModel
+//import com.example.cuddlecare.ui.viewmodel.ProfileViewModel
+//
+//enum class AuthenticatedLandingScreen {
+//    setup,
+//    Application
+//}
+//@Composable
+//fun AuthenticatedLandingScreen(
+//    firebaseAuthViewModel: FirebaseAuthViewModel = viewModel(),
+//){
+//    //val profileUiState by ProfileViewModel.profileUiState.collectAsState()
 //    val navHostController = rememberNavController()
 //
 //    LaunchedEffect(Unit ) {
-//        profileViewmodel.getUserProfile(firebaseAuthViewModel.currentUser()!!)
+//  //      ProfileViewModel.getUserProfile(firebaseAuthViewModel.currentUser()!!)
 //    }
 //    if (profileUiState.loading) {
 //        Column(modifier = Modifier.padding(start = 16.dp,end =16.dp)){
@@ -47,25 +48,25 @@ fun AuthenticatedLandingScreen(
 //                SetupScreen(
 //                    firebaseAuthViewModel = firebaseAuthViewModel,
 //                    topNavController = topNavHostController,
-//                    profileViewmodel = profileViewmodel
+//                    profileViewmodel = ProfileViewModel
 //                )
 //
 //            }
 //        }
 //        composable(AuthenticatedLandingScreen.Application.name){
-//            ApplicationScreen()
+//            //Screen.ApplicationScreen()
 //        }
 //    }
-}
-
-@Composable
-private fun Loading(){
-    Column(
-    modifier = Modifier.fillMaxSize(),
-    verticalArrangement = Arrangement.Center,
-    horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        CircularProgressIndicator()
-    }
-
-}
+//}
+//
+//@Composable
+//private fun Loading(){
+//    Column(
+//    modifier = Modifier.fillMaxSize(),
+//    verticalArrangement = Arrangement.Center,
+//    horizontalAlignment = Alignment.CenterHorizontally
+//    ){
+//        CircularProgressIndicator()
+//    }
+//
+//}
