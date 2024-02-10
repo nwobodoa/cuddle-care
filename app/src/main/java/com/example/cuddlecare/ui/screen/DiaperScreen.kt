423267dadf35896b105e016b9110b7854d516f8a
Adanwa Nwobodo <adanwanwobodo@gmail.com>
2024-02-10 03:05:37 -0800

    implemented the diaper screen

--- app/src/main/java/com/example/cuddlecare/ui/screen/DiaperScreen.kt
+++ app/src/main/java/com/example/cuddlecare/ui/screen/DiaperScreen.kt
@@ -16,9 +16,14 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.DropdownMenuItem
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.ExposedDropdownMenuBox
+import androidx.compose.material.ExposedDropdownMenuDefaults
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccessTime
 import androidx.compose.material.icons.filled.Add
@@ -33,119 +38,232 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardCapitalization
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.cuddlecare.R
+import com.example.cuddlecare.ui.components.TopBar
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
 @Preview(showBackground = true)
 fun add_diaper_record() {
+
+
     Column(
         modifier = Modifier
-            .clip(shape = RoundedCornerShape(20.dp))
-    )
-    {
-        Row {
-            Image(
-                painter = painterResource(id = R.drawable.diaper_logo),
-                contentDescription = null,
-                Modifier
-                    .size(350.dp)
-                    .padding(bottom = 32.dp, top = 32.dp),
-                contentScale = ContentScale.Crop
-            )
-        }
-        Text(text = "Diaper", fontSize = 28.sp, fontWeight = FontWeight.Bold)
-        Text(text = "Last: 20mins ago")
-        Column(modifier = Modifier.background(color = Color.White)) {
-            Row {
-                Icon(Icons.Default.AccessTime, contentDescription = "Timer")
-                Text(text = "Time")
-                Text(text = "9 Nov")
-                Text(text = "2:44 AM")
-            }
-            Row {
-                var value by remember { mutableStateOf("") }
-                var mExpanded by remember { mutableStateOf(false) }
-                val groups = listOf("Disposable", "Cloth")
-                var mTextFieldSize by remember { mutableStateOf(Size.Zero) }
+            .fillMaxHeight()
+            .background(color = colorResource(id = R.color.orange))
+    ) {
+        TopBar()
+        Column(
+            modifier = Modifier
+                .fillMaxHeight()
+                .clip(shape = RoundedCornerShape(topStart = 40.dp, topEnd = 40.dp))
+                .background(color = colorResource(id = R.color.backcolor))
+                .padding(16.dp)
+        )
+        {
 
-                Icon(Icons.Outlined.Folder, contentDescription = "Folder icon")
-                TextField(value = value, onValueChange = { value = it },
-                    label = { Text(text = "Select group") }
-                )
-                //TODO add drop down menu
-
-            }
             Row(
-                horizontalArrangement = Arrangement.End,
-                modifier = Modifier.fillMaxWidth()
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 4.dp)
             ) {
-                Button(onClick = { /*TODO*/ }) {
-                    Icon(
-                        imageVector = Icons.Default.WaterDrop,
-                        contentDescription = "water drop"
-                    )
-                    Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
-                    Text(text = "Wet")
+                Image(
+                    painter = painterResource(id = R.drawable.diaper_logo),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(200.dp),
+                    contentScale = ContentScale.Crop
+                )
+            }
+            Text(text = "Diaper", fontSize = 28.sp, fontWeight = FontWeight.Bold)
+            Text(text = "Last: 20mins ago")
+            Column(
+                modifier = Modifier
+                    .clip(shape = RoundedCornerShape(20.dp))
+                    .background(color = Color.White)
+                    .padding(8.dp)
+            )
 
+            {
+                Row {
+                    Icon(Icons.Default.AccessTime, contentDescription = "Timer")
+                    Text(text = "Time")
+                    Text(text = "9 Nov")
+                    Text(text = "2:44 AM")
                 }
-                Button(onClick = { /*TODO*/ }, modifier = Modifier.padding(start = 8.dp)) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.poop),
-                        contentDescription = "Poop",
-                        tint = Color.Black
-                    )
-                    Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
-                    Text(text = "Dry")
+                Row {
+
+                    val groups = listOf("Disposable", "Cloth")
+                    var selectedIndex by remember { mutableStateOf(0) }
+
+                    Icon(Icons.Outlined.Folder, contentDescription = "Folder icon")
+                    ExposedSelectionMenu(title = "Select group", options =groups , onSelected ={
+                        index -> selectedIndex = index
+                    } )
+                }
+
+                Row(
+                    horizontalArrangement = Arrangement.End,
+                    modifier = Modifier.fillMaxWidth()
+
+                ) {
+                    var isClicked by remember { mutableStateOf(false)}
+                    var isEnabled by remember { mutableStateOf(true)}
+                    Button(
+                        onClick = { isEnabled = !isEnabled},
+                        enabled = !isEnabled
+
+
+                    ) {
+
+                        Icon(
+                            imageVector = Icons.Default.WaterDrop,
+                            contentDescription = "water drop"
+                        )
+                        Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
+                        Text(text = "Wet")
+
+                    }
+                    Button(onClick = {
+                           isClicked = !isClicked
+                    }, enabled = isClicked, modifier = Modifier.padding(start = 8.dp)) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.poop),
+                            contentDescription = "Poop",
+                            tint = Color.Black
+                        )
+                        Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
+                        Text(text = "Dry")
+                    }
                 }
             }
-        }
-        Row(horizontalArrangement = Arrangement.End) {
-            Text(text = "Count : 0")
-            Spacer(modifier = Modifier.size(6.dp))
-            Icon(imageVector = Icons.Filled.Add, contentDescription = "Plus icon")
-        }
-        AttachmentRow()
-        SaveButton()
+            Row() {
+                Text(text = "Count : 0")
+                Spacer(modifier = Modifier.size(6.dp))
+                Icon(imageVector = Icons.Filled.Add, contentDescription = "Plus icon")
+            }
+            AttachmentRow()
+            SaveButton()
+            //TODO disable save button if nothing is entered
 
-
+        }
     }
 }
 
 @Composable
 fun AttachmentRow() {
-    var inputValue by remember { mutableStateOf(TextFieldValue()) }
-              TextField(
-                value = inputValue,
-                onValueChange = { inputValue = it },
-                label = { Text(text = "Notes") },
-//                  inactiveColor = Color.Gray,
-//                KeyboardOptions = KeyboardOptions(
-//                    capitalization = KeyboardCapitalization.None,
-//                    autoCorrect = true,)
-            )
-        Row {
-            Text(text = "Attachments")
-            Icon(
-                painter = painterResource(id = R.drawable.img),
-                contentDescription = "Gallery icon"
-            )
-            Icon(imageVector = Icons.Default.CameraAlt, contentDescription = "Camera icon")
-        }
+    val maxLength = 3000
+    var inputValue by remember { mutableStateOf("") }
 
+    TextField(
+        modifier = Modifier.fillMaxWidth(),
+        value = inputValue,
+        onValueChange = {
+            if (it.length <= maxLength) inputValue = it
+        },
+
+        colors = TextFieldDefaults.textFieldColors(
+            backgroundColor = colorResource(id = R.color.backcolor),
+            unfocusedIndicatorColor = Color.Gray,
+            focusedIndicatorColor = Color.Transparent,
+            disabledIndicatorColor = Color.Transparent,
+        ),
+        keyboardOptions = KeyboardOptions(autoCorrect = true),
+        label = { Text(text = "Notes") },
+
+        )
+    Text(
+        text = "${inputValue.length}/$maxLength",
+        textAlign = TextAlign.End,
+        color = Color.Gray,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(end = 16.dp)
+    )
+
+    Row(modifier = Modifier.fillMaxWidth()) {
+        Text(text = "Attachments", modifier = Modifier.weight(1f))
+        Icon(
+            painter = painterResource(id = R.drawable.img),
+            contentDescription = "Gallery icon",
+            modifier = Modifier.align(Alignment.CenterVertically)
+
+        )
+        Icon(
+            imageVector = Icons.Default.CameraAlt, contentDescription = "Camera icon",
+            modifier = Modifier.align(Alignment.CenterVertically)
+        )
     }
+
+}
+
+@Composable
+@OptIn(ExperimentalMaterialApi::class)
+fun ExposedSelectionMenu(
+    title: String,
+    options: List<String>,
+    onSelected: (Int) -> Unit
+) {
+
+    var expanded by remember { mutableStateOf(false) }
+    var selectedOptionText by remember { mutableStateOf("") }
+    ExposedDropdownMenuBox(
+        modifier = Modifier
+            .padding(vertical = 4.dp),
+        expanded = expanded,
+        onExpandedChange = {
+            expanded = !expanded
+        }
+    ) {
+        TextField(
+            readOnly = true,
+            value = selectedOptionText,
+            onValueChange = { },
+            trailingIcon = {
+                ExposedDropdownMenuDefaults.TrailingIcon(
+                    expanded = expanded
+                )
+            },
+            placeholder = { Text(text = title)},
+            colors = ExposedDropdownMenuDefaults.textFieldColors(
+                backgroundColor = Color.White,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Gray,
+                disabledIndicatorColor = Color.Transparent,
+            )
+
+        )
+        ExposedDropdownMenu(
+             expanded = expanded,
+            onDismissRequest = {
+                expanded = false
+
+            }
+        ) {
+            options.forEachIndexed { index: Int, selectionOption: String ->
+                DropdownMenuItem(
+                     onClick = {
+                        selectedOptionText = selectionOption
+                        expanded = false
+                        onSelected(index)
+                    }
+                ) {
+                    Text(text = selectionOption)
+                }
+            }
+        }
+    }
+}
 
 
 
