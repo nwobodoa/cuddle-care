43bd8660559bd88d727a2c55ed9acd61a86838f1
Ada Nwobodo <adanwanwobodo@gmail.com>
2024-02-14 23:45:33 -0800

    Sleeping record UI

---
+++ app/src/main/java/com/ebony/cuddlecare/ui/screen/SleepScreen.kt
@@ -0,0 +1,145 @@
+package com.ebony.cuddlecare.ui.screen
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Flag
+import androidx.compose.material.icons.filled.Pause
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.Timelapse
+import androidx.compose.material.icons.filled.Timer
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.ebony.cuddlecare.R
+import com.ebony.cuddlecare.ui.components.ToggableButton
+import com.ebony.cuddlecare.ui.components.mTopBar
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Preview
+@Composable
+fun SleepingScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxHeight()
+            .background(color = colorResource(id = R.color.orange))
+    )
+    {
+        mTopBar()
+        Column(
+            modifier = Modifier
+                .fillMaxHeight()
+                .clip(shape = RoundedCornerShape(topStart = 40.dp, topEnd = 40.dp))
+                .background(color = colorResource(id = R.color.backcolor))
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            ScreenMainIcon(R.drawable.sleep_logo)
+            Row(modifier = Modifier.fillMaxWidth(),horizontalArrangement = Arrangement.SpaceBetween) {
+                Row {
+                    LastUpdated("Sleeping")
+                }
+                Row {
+                    Icon(imageVector = Icons.Default.Add, contentDescription = "Add icon")
+                    Icon(imageVector = Icons.Default.Delete, contentDescription =" Delete icon" )
+                }
+
+            }
+
+            Column(
+                modifier = Modifier
+                    .clip(shape = RoundedCornerShape(20.dp))
+                    .background(color = Color.White)
+                    .padding(16.dp)
+                    .fillMaxWidth(),
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            )
+
+            {
+                Row(modifier = Modifier
+                    .fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+                   ToggableButton(activated = false, onClick = { /*TODO*/ }) {
+                       Icon(imageVector = Icons.Default.PlayArrow, contentDescription = "play icon")
+                       Icon(imageVector = Icons.Default.Pause, contentDescription = "play icon")
+                       Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
+                       Text(text = "Resume")
+                   }
+                }
+            }
+            Column (modifier = Modifier
+                .clip(shape = RoundedCornerShape(20.dp))
+                .background(color = Color.White)
+                .padding(16.dp)
+                .fillMaxWidth(),
+                verticalArrangement = Arrangement.spacedBy(16.dp))
+            {
+
+
+                Row {
+                    LeadingDetailsIcon(title = "Duration", imageVector = Icons.Default.Timelapse,
+                        contentDescription = "timeelapsed icon" )
+                    Row(
+                        horizontalArrangement = Arrangement.End,
+                        modifier = Modifier.weight(1f)
+                    ) {
+                        Text(text = "1 min 50 s")
+                    }
+                }
+                Row {
+                    LeadingDetailsIcon(title = "Pause", imageVector = Icons.Default.Pause,
+                        contentDescription = "pause icon" )
+                    Row(
+                        horizontalArrangement = Arrangement.End,
+                        modifier = Modifier.weight(1f)
+                    ) {
+                        Text(text = "0 s")
+                    }
+                }
+                Row {
+                    LeadingDetailsIcon(title = "Started", imageVector = Icons.Default.Timer,
+                        contentDescription = "timer icon" )
+                    Row(
+                        horizontalArrangement = Arrangement.End,
+                        modifier = Modifier.weight(1f)
+                    ) {
+                        Text(text = "9 Nov ")
+                        Text(text = " 2:44 AM")
+                    }
+                }
+                Row {
+                    LeadingDetailsIcon(title = "Ended", imageVector = Icons.Default.Flag,
+                        contentDescription = "timer icon" )
+                    Row(
+                        horizontalArrangement = Arrangement.End,
+                        modifier = Modifier.weight(1f)
+                    ) {
+                        Text(text = "9 Nov ")
+                        Text(text = " 2:44 AM")
+                    }
+                }
+            }
+            AttachmentRow()
+            SaveButton()
+        }
+    }
+}
+
